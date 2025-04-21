6b4387d50fad11e3705b7a133bf960cc154eb9f7
VktYe <v.yehorenko@gmail.com>
2025-04-21 15:25:26 -0300

    Implement renderTweets function. Responsible for taking in an array of tweet objects and then appending each one to the #tweets-container.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,31 +1,54 @@
 $(document).ready(function() { //
 
-  const  tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-  const createTweetElement = function() {
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+      };
+      //takes in an array of tweet objects
+      //calls createTweetElement for each tweet
+      //then appends each one to the #tweet-container
+    };
+
+  const createTweetElement = function(tweet) {
     const $tweet = (`
       <section class="tweet">
         <header class="user-info">
           <div class="avatar-name">
-            <img src="${tweetData.user.avatars}"/>
-            <span>${tweetData.user.name}</span>
+            <img src="${tweet.user.avatars}"/>
+            <span>${tweet.user.name}</span>
           </div>
-            <span>${tweetData.user.handle}</span> 
+            <span>${tweet.user.handle}</span> 
         </header>
-        <article>${tweetData.content.text}</article>
+        <article>${tweet.content.text}</article>
         <footer>
           <div>
-            <span>${tweetData.created_at}</span>
+            <span>${tweet.created_at}</span>
           </div>
           <div class="icons">
             <i class="fas fa-bookmark"></i>
@@ -38,6 +61,10 @@ $(document).ready(function() { //
 
       return $tweet;  
     };
-    const $tweet = createTweetElement(tweetData);
-    $('#tweets-container').append($tweet);
+
+
+  renderTweets(data);
+    
 });
+
+
