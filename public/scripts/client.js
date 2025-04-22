ea2833474446ac1b1e2662a441dd2a6acaf1112d
VktYe <v.yehorenko@gmail.com>
2025-04-22 07:14:57 -0300

    Implement loadTweets function that fetches tweets from the http://localhost:8080/tweets page. Use timeago format in

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,30 +1,5 @@
 $(document).ready(function() {
-
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-
+  
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
@@ -45,7 +20,7 @@ $(document).ready(function() {
         <article>${tweet.content.text}</article>
         <footer>
           <div>
-            <span>${tweet.created_at}</span>
+            <span>${timeago.format(tweet.created_at)}</span>
           </div>
           <div class="icons">
             <i class="fas fa-bookmark"></i>
@@ -58,8 +33,6 @@ $(document).ready(function() {
 
       return $tweet;  
     };
-
-  renderTweets(data); 
   
   // Form Submition
   $(".tweet-form").on("submit", function(event) {
@@ -76,6 +49,17 @@ $(document).ready(function() {
       }
     });
   });
+
+  const loadTweets = function() {
+    $.ajax("/api/tweets", { method: 'GET' })
+     .then(function(tweets) {
+       console.log('Success: ', tweets);
+       renderTweets(tweets);
+     });
+  };
+
+  loadTweets();
+ 
   
   const serializedData = $(this).serialize();
 });
